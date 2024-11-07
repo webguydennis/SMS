2b45c26ea87d60df500cc99eb9858346b58a4fbb
Dennis Estrada <air_mvp_jordan@yahoo.com>
2024-11-06 20:28:25 -0800

    Add CORS Policy and fix the Ok test

--- TextGateKeeper/Program.cs
+++ TextGateKeeper/Program.cs
@@ -8,6 +8,22 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// CORS Policy
+builder.Services.AddCors((options) => {
+    options.AddPolicy("DevCors", (corsBuilder) => {
+        corsBuilder.WithOrigins("http://localhost:4200", "http://localhost:3000", "http://localhost:8000")
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials();
+    });
+    options.AddPolicy("ProdCors", (corsBuilder) => {
+        corsBuilder.WithOrigins("https://myProduction.com")
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials();
+    });
+});
+
 builder.Services.AddScoped<ITextMessageRepository, TextMessageRepository>();
 
 var app = builder.Build();
@@ -15,9 +31,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseCors("DevCors");
     app.UseSwagger();
     app.UseSwaggerUI();
-} else {
+} else 
+    {app.UseCors("ProdCors");
     app.UseHttpsRedirection();
 }
 
